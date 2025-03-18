eeb70d99ff00800c0a59b4546a4d0bf1cb8e2bbf
Hinkley <Se.G.Hinkley@baypath.net>
2025-03-18 14:17:56 -0400

    Tiles Render

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const io = socketIO(server);
 
 const connectionString = "mongodb+srv://seanhinkley567:hi6yIe40@school.9eciq.mongodb.net/?retryWrites=true&w=majority&appName=School";
 
-
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+    socket.on("buttonPressed", (data) => {
+        console.log(data);
+    })
+})
 
 
 
--- public/board.js
+++ public/board.js
@@ -1,15 +1,77 @@
-
+const socket = io();
 let tilerow;
-const cont = document.querySelector("container");
-for(var x = 0; x < 7; x++) {
+let tile;
+const cont = document.querySelector(".holder");
+
+for(var x = 0; x < 6; x++) {
+
     tilerow = document.createElement("div");
-    tilerow.setAttribute("class", "row m-auto");
-    console.log(tilerow);
+    tilerow.setAttribute("class", "row m-auto justify-content-center");
+    
     cont.appendChild(tilerow);
-    for(var y = 0; y < 6; y++) {
-        let tile = document.createElement("div");
-        tile.setAttribute("class", "col-2 block bg-primary m-2");
+    for(var y = 0; y < 7; y++) {
+        tile = document.createElement("div");
+        tile.setAttribute(`class`, `border-3 border border-dark mx-3 col-2 block bg-primary-subtle m-1 `);
+        tile.setAttribute("id", `O-${x}-${y}`);
         tilerow.appendChild(tile);
     }
     
 }
+
+const blocks = document.querySelectorAll(".block");
+for(var s = 0; s < blocks.length; s++) {
+    let f = blocks[s];
+    f.addEventListener('click', (event) => {
+        let c = f.getAttribute('id');
+        let n = c.charAt(4);
+        socket.emit("buttonPressed",  n);
+        console.log(n);
+
+    });
+}
+
+class Block {
+
+    constructor(x,y) {
+        this.div = document.createElement('div');
+        this.div.setAttribute('class', `block col-2 m-1 bg-danger mx-auto border border-3 border-dark`)
+    }
+}
+
+class Board {
+    constructor() {
+        this.board = [];
+        this.createBoard();
+    }
+
+    createBoard() {
+        for(var x = 0; x < 6; x++) {
+            this.board.push([]);
+            let n = document.createElement('div');
+            n.setAttribute('class', "row m-auto  justify-content-center");
+            this.board[x].push(n);
+            for(var y = 0; y < 7; y++) {
+                let c = new Block(x,y);
+                this.board[x].push(c.div);
+            }
+        }
+    }
+
+
+
+    addBoard(container) {
+        for(var x = 0; x < this.board.length; x++) {
+            let b = this.board[x][0];
+            container.appendChild(b);
+            for(var y = 1; y < this.board[x].length; y++) {
+                let n = this.board[x][y];
+                b.appendChild(n);
+            }
+        }
+        
+    }
+}
+
+let B = new Board();
+
+B.addBoard(cont);
